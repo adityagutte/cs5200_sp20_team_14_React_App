863c3e1a4058fd64e0e705d6e47d881d1e416387
adityagutte <adityagutte@gmail.com>
2020-04-21 20:23:30 -0400

    added build

---
+++ build/precache-manifest.7b64adb8e6ad4588a617ea9f288e57c4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "799ff411c5b678a1a7d526c78be98b04",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bca238748b487648f249",
+    "url": "/static/css/2.b508fae9.chunk.css"
+  },
+  {
+    "revision": "cf011effd081ad9e3e83",
+    "url": "/static/css/main.417b789d.chunk.css"
+  },
+  {
+    "revision": "bca238748b487648f249",
+    "url": "/static/js/2.4c225231.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.4c225231.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf011effd081ad9e3e83",
+    "url": "/static/js/main.1f0b43c3.chunk.js"
+  },
+  {
+    "revision": "a67814ebcba6171f2881",
+    "url": "/static/js/runtime-main.7e65e7b7.js"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/static/media/notification.c0d3c94c.eot"
+  }
+]);
